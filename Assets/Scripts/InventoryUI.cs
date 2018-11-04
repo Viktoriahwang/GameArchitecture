ab7983eafc265269454c45259ceff6fbf6c23f05
Viktoria <viktoria@MacBook-Pro.local>
2018-11-04 06:13:49 -0500

    Buy and Use inventory
    Add inventory from shop to players inventory.
    Remove (aka Use) inventory from player inventory

--- Assets/Scripts/InventoryUI.cs
+++ Assets/Scripts/InventoryUI.cs
@@ -1,22 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class InventoryUI : MonoBehaviour {
+
+
+public class InventoryUI : MonoBehaviour
+{
 
     public Inventory inventory;
     public Transform content;
     public ItemUI itemUIPrefab;
 
-	// Use this for initialization
-	void Start () {
-        if (inventory)
+    public bool showPlayerInventory;
+
+    public ItemEvent itemSelected;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (showPlayerInventory)
+        {
+            Display(Inventory.playerInventory);
+        }
+        else if (inventory)
             Display(inventory);
-	}
-	
+    }
+
     public virtual void Display(Inventory i)
     {
-        if (this.inventory) 
+        if (this.inventory)
         {
             this.inventory.onChanged.RemoveListener(Refresh);
         }
@@ -28,7 +41,7 @@ public class InventoryUI : MonoBehaviour {
     //visual refresh. Changes itemUI stuff
     public virtual void Refresh()
     {
-        foreach(Transform t in content)
+        foreach (Transform t in content)
         {
             Destroy(t.gameObject);
         }
@@ -43,9 +56,32 @@ public class InventoryUI : MonoBehaviour {
 
     public virtual void UIClicked(ItemUI iui)
     {
-        Inventory.playerInventory.Add(iui.item);
-        inventory.Remove(iui.item);
+        itemSelected.Invoke(iui.item);
+
+        //Inventory.playerInventory.Add(iui.item);
+        //inventory.Remove(iui.item);
 
         Destroy(iui.gameObject);
     }
+
+    //Code referenced by UnityEvents only
+    #region UnityEventsResponders
+
+    public void AddToPlayerInventory (Item item)
+    {
+        Inventory.playerInventory.Add(item);
+    }
+
+    public void RemoveFromOwnInventory (Item item)
+    {
+        inventory.Remove(item);
+    }
+
+    public void Use (Item item)
+    {
+        if (!item.Use())
+            RemoveFromOwnInventory(item);
+    }
+
+    #endregion
 }
--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class ItemEvent : UnityEvent<Item> { }
 
 public class Item : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public bool consumable = true;
+
+    //returns true if item is kept, false if destroyed
+    public bool Use()
+    {
+        return !consumable;
+    }
 }
